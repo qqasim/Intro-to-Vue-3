0dbe54762b9eaaeefb32b79ef79546dd63552f91
Qasim Qasim <qq4fd@virginia.edu>
2021-06-10 14:46:01 -0400

    Changed description L1 done

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const app = Vue.createApp({
     data() {
         return {
             product : 'Boots',
-            description : 'These boots are durable and built to last!'
+            description : 'These boots are durable and built to withstand any wear and tear!'
         }
     }
 })
