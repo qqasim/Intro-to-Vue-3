6de225432724bde4ed34f6cdec39205df812de6e
Qasim Qasim <qq4fd@virginia.edu>
2021-06-13 17:41:45 -0400

    L10 done

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -5,7 +5,7 @@ app.component('product-display', {
       required: true
     }
   },
-  template: 
+  template:
   /*html*/
   `<div class="product-display">
     <div class="product-container">
@@ -24,21 +24,30 @@ app.component('product-display', {
           <li v-for="detail in details">{{ detail }}</li>
         </ul>
 
-        <div 
-          v-for="(variant, index) in variants" 
-          :key="variant.id" 
-          @mouseover="updateVariant(index)" 
-          class="color-circle" 
+        <div
+          v-for="(variant, index) in variants"
+          :key="variant.id"
+          @mouseover="updateVariant(index)"
+          class="color-circle"
           :style="{ backgroundColor: variant.color }">
         </div>
-        
-        <button 
-          class="button" 
-          :class="{ disabledButton: !inStock }" 
-          :disabled="!inStock" 
+
+        <button
+          class="button"
+          :class="{ disabledButton: !inStock }"
+          :disabled="!inStock"
           v-on:click="addToCart">
           Add to Cart
         </button>
+
+        <button
+          class="button"
+          :class="{ disabledButton: !inStock }"
+          :disabled="!inStock"
+          v-on:click="removeFromCart">
+          Remove from Cart
+        </button>
+
       </div>
     </div>
   </div>`,
@@ -50,17 +59,23 @@ app.component('product-display', {
         details: ['50% cotton', '30% wool', '20% polyester'],
         variants: [
           { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-          { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+          { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 2 },
         ]
     }
   },
   methods: {
       addToCart() {
-          this.cart += 1
+          this.$emit('add-to-cart', this.variants[this.selectedVariant].id)
       },
+    //   removeFromCart() {
+    //       this.$emit('remove-from-cart', this.variants[this.selectedVariant].id)
+    //   },
       updateVariant(index) {
           this.selectedVariant = index
-      }
+      },
+      removeFromCart() {
+          this.$emit('remove-from-cart', this.variants[this.selectedVariant].id)
+      },
   },
   computed: {
       title() {
